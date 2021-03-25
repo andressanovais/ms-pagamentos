a136d7a794f3594831fb2e4218c84b0b74d940c1
Andressa Novais <andressa.novais@zappts.com>
2021-03-25 01:20:28 -0300

    feat: Incluindo call à interface de processamento

--- src/apresentacao/controllers/pagamento/pagamento.ts
+++ src/apresentacao/controllers/pagamento/pagamento.ts
@@ -1,6 +1,7 @@
-import { HttpRequest, HttpResponse } from '../../protocolos'
-import { badRequest, internalServerError, ok } from '../../helpers'
-import { InternalServerError, ParamInvalidosErro, ParamObrigatorioErro } from '../../erros'
+import { HttpResponse, Controller, Validador } from '@/apresentacao/protocolos'
+import { badRequest, internalServerError, ok } from '@/apresentacao/helpers'
+import { ProcessadorPagamento } from '@/dominio/casos-uso'
+import { InternalServerError } from '@/apresentacao/erros'
 
 export class PagamentoController implements Controller {
   constructor (
@@ -8,14 +9,32 @@ export class PagamentoController implements Controller {
     private readonly processadorPagamento: ProcessadorPagamento
   ) {}
 
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle (request: PagamentoController.Request): Promise<HttpResponse> {
     try {
-      const resultadoValidacao = await this.validador.validar(httpRequest.body)
-      if (resultadoValidacao.error) {
-        return badRequest(resultadoValidacao.error)
+      const resultadoValidacao = await this.validador.validar(request)
+      if (resultadoValidacao.erro) {
+        return badRequest(resultadoValidacao.erro)
       }
+
+      const mensagem = await this.processadorPagamento.processar(request)
+
+      return ok(mensagem)
     } catch (erro) {
-      return internalServerError(erro)
+      return internalServerError(new InternalServerError())
     }
+  }
+}
+
+export namespace PagamentoController {
+  export type Request = {
+    divida: Divida
+    idConta: number
+  }
+
+  type Divida = {
+    id: number
+    valor: string
+    formaPagamento: string
+    ativa: boolean
   }
 }
