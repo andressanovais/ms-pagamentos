dbaf9e738817c54f5077699b1a7bc6fdc4473602
Andressa Novais <andressa.novais@zappts.com>
2021-03-24 23:37:23 -0300

    feat: Http status

---
+++ src/apresentacao/helpers/http-status.ts
@@ -0,0 +1,16 @@
+import { HttpResponse } from '../protocolos'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const internalServerError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: error
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
