63a8e1caaff13b0bcd9e4a83764118a72154928b
Andressa Novais <andressa.novais@zappts.com>
2021-03-27 19:45:58 -0300

    feat: JoiValidatorAdapter

---
+++ src/infra/helpers/verifica-pagamento.ts
@@ -0,0 +1,9 @@
+import { PagamentoModel } from '@/domain/models';
+
+export const ehPagamento = (data: any) : data is PagamentoModel => {
+  if ((data as PagamentoModel).divida) {
+    return true
+  } else {
+    return false
+  }
+} 
---
+++ src/infra/validators/joi-validator-adapter.ts
@@ -0,0 +1,37 @@
+import Joi from 'joi'
+import { Validator, RespostaValidacao } from '@/presentation/protocols'
+import { PagamentoModel, EfetivacaoBoletoModel } from '@/domain/models'
+import { ehPagamento } from '@/infra/helpers/verifica-pagamento'
+
+export class JoiValidatorAdapter implements Validator {
+  validar (dados: PagamentoModel | EfetivacaoBoletoModel): RespostaValidacao {
+    const schema = this.selecionarSchema(dados)
+    const { error } = schema.validate(dados)
+
+    if (error) {
+      return new RespostaValidacao(true)
+    } else {
+      return new RespostaValidacao(false, error)
+    }
+  }
+
+  selecionarSchema (dados: PagamentoModel | EfetivacaoBoletoModel): Joi.ObjectSchema {
+    if (ehPagamento(dados)) {
+      return pagamentoSchema
+    } else {
+      return efetivacaoBoletoSchema
+    }
+  }
+}
+
+const pagamentoSchema = Joi.object().keys({
+  idConta: Joi.number().positive().required(),
+  divida: Joi.object().keys({
+    id: Joi.number().positive().required(),
+    valor: Joi.number().required(),
+    formaPagamento: Joi.string().valid('DEBITO_CONTA', 'CREDITO', 'BOLETO').required()
+  })
+})
+
+const efetivacaoBoletoSchema = Joi.object().keys({
+})
