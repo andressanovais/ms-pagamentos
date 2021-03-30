cc10b75deeee1b07253926a4947ffec086a6f744
Andressa Novais <andressa.novais@zappts.com>
2021-03-30 12:38:57 -0300

    update: Alterando método permitido para patch

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -3,6 +3,6 @@ import { Request, Response, NextFunction } from 'express'
 export const cors = (req: Request, res: Response, nextFunction: NextFunction): void => {
   res.set('access-control-allow-origin', '*')
   res.set('access-control-allow-headers', '*')
-  res.set('access-control-allow-methods', 'POST')
+  res.set('access-control-allow-methods', 'PATCH')
   nextFunction()
 }
